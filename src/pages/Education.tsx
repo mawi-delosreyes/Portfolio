6851e04dff543d42b837b037bf3471716c665ead
mawi-delosreyes <mwldelosreyes13@gmail.com>
2025-07-25 05:09:50 +0800

    Added university in education

--- src/pages/Education.tsx
+++ src/pages/Education.tsx
@@ -1,10 +1,20 @@
 import '../styles/education.css';
+import universityIcon from '../assets/images/mapua.png';
+import udemyIcon from '../assets/images/udemy.png';
 
 function EducationPage() {
     return (
         <div className="background-container">
-            <div className='background-squares'/>
-            <div className='background-squares'/>
+            <div className='background-squares'>
+                <img id="university-icon" src={universityIcon}/>
+                <div id='university-text'>
+                    <h4>Bachelor of Science in Computer Science</h4>
+                    <h5>Specialization in Artificial Intelligence</h5>
+                </div>
+            </div>
+            <div className='background-squares'>
+                <img id="udemy-icon" src={udemyIcon}/>
+            </div>
         </div>
     );
 }
