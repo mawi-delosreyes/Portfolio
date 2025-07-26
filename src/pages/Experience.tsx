f3999d2ba936e4c56e712e68c1f8ee3fe0028e15
mawi-delosreyes <mwldelosreyes13@gmail.com>
2025-07-27 06:01:48 +0800

    feat: added descriptions

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -12,6 +12,48 @@ function ExperiencePage() {
         { role: "Software Engineer - Data Science Intern", company: "Realtair Inc.", period: "2021 to 2021", top: '90%' }
     ];
 
+    const experienceDetails = [
+        [
+          "Develop and maintain backend services using Java, Node.js, and PHP for consumer-facing web and mobile platforms (iOS & Android)",
+          "Support 6 million monthly Australian active users by ensuring and maintaining backend infrastructure",
+          "Review and address customer support issues",
+          "Ensure functionality of the CMS tool used by meteorologists to publish weather news content",
+          "Integrate Apple News API, increasing content delivery efficiency for meteorologists",
+          "Troubleshoot and monitor system performance using Datadog; resolve incident and support tickets",
+          "Refactor and modernize legacy codebases for improved maintainability",
+          "Implement CI/CD pipelines via GitLab and manage version control using GitLab",
+          "Collaborate with cross-functional teams and stakeholders across departments",
+          "Produce technical documentation, architecture diagrams, and research reports for backend systems"
+        ],
+        [
+          "Developed and maintained Python Flask-based APIs integrated with websites and mobile apps, processing incoming data from IoT hardware across 15+ solar project sites and over 50 devices",
+          "Designed and implemented relational database schemas using MySQL",
+          "Optimized and refactored backend codebases for scalability and maintainability",
+          "Managed cloud infrastructure using AWS services: EC2, IAM, Lambda, RDS, S3, SES, VPC, and CloudWatch",
+          "Integrated payment gateways and weather APIs for internal systems",
+          "Authored internal backend coding standards and comprehensive technical documentation",
+          "Led 2 major in-house projects (Envision Monitoring and Envision EV)"
+        ],
+        [
+          "Developed and maintained Python Flask-based APIs integrated with websites and mobile apps, processing incoming data from IoT hardware across 15+ solar project sites and over 50 devices",
+          "Designed and implemented relational database schemas using MySQL",
+          "Optimized and refactored backend codebases for scalability and maintainability",
+          "Managed cloud infrastructure using AWS services: EC2, IAM, Lambda, RDS, S3, SES, VPC, and CloudWatch",
+          "Integrated payment gateways and weather APIs for internal systems",
+          "Authored internal backend coding standards and comprehensive technical documentation",
+          "Led 2 major in-house projects (Envision Monitoring and Envision EV)"
+        ],
+        [
+          "Built and maintained websites using PHP, HTML, CSS, and JavaScript",
+          "Created automation scripts using Puppeteer for backend data operations",
+          "Integrated REST APIs for database communication"
+        ],
+        [
+          "Analyzed and organized structured data from internal databases",
+          "Delivered actionable insights to stakeholders through data-driven presentations"
+        ]
+      ];
+
     return (
         <div id="experience">
 
@@ -21,6 +63,12 @@ function ExperiencePage() {
                         <h3>{experiences[activeIndex].company}</h3>
                         <h4>{experiences[activeIndex].role}</h4>
                         <p>{experiences[activeIndex].period}</p>
+
+                        <ul>
+                            {experienceDetails[activeIndex].map((detail, index) => (
+                                <li key={index}>{detail}</li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
             ) : (
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -4,32 +4,32 @@ import ProjectCard from '../components/ProjectCard';
 function ProjectsPage() {
     const projectList = [
         {
-            title: "My Project 1",
-            githubUrl: "sample url",
-            description: "sample description",
-            languages: ["python"],
+            title: "PokeSet",
+            githubUrl: "https://github.com/mawi-delosreyes/PokeSet",
+            description: "A Pokemon pokedex and VGC team builder where users can view pokemon details, save their team layouts, and see analytics and team coverages. ",
+            languages: ["Java", "React", "TypeScript"],
             database: "MySQL"
         },
         {
-            title: "My Project 2",
-            githubUrl: "another url",
-            description: "another description",
-            languages: ["typescript", "react"],
-            database: "PostgreSQL"
+            title: "Trakua",
+            githubUrl: "https://github.com/mawi-delosreyes/Trakua",
+            description: "A web and mobile application that allows users to track their financial habits and goals. This focuses on the flexibility of tracking from multiple sources.",
+            languages: ["Flutter", "Dart"],
+            database: "MySQL, SQLite"
         },
         {
-            title: "My Project 3",
-            githubUrl: "another url",
-            description: "another description",
-            languages: ["typescript", "react"],
-            database: "PostgreSQL"
+            title: "Harvest",
+            githubUrl: "https://github.com/mawi-delosreyes/Harvest",
+            description: "A cryptocurrency algorithmic trader for Coins.ph platform. This connects to their API to execute trades from a 1GB - RAM Raspberry Pi 4 that runs 24/7.",
+            languages: ["Python"],
+            database: "MySQL"
         },
         {
-            title: "My Project 4",
-            githubUrl: "another url",
-            description: "another description",
-            languages: ["typescript", "react"],
-            database: "PostgreSQL"
+            title: "Kards",
+            githubUrl: "https://github.com/mawi-delosreyes/Kards",
+            description: "A Baccarat mobile application that tracks game progress and provides analytics for the current turn.",
+            languages: ["Dart", "Flutter"],
+            database: "SQLite"
         }
     ];
 
