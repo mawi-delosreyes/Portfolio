b340ede23e37d367a68ebdefd40e899044838be6
mawi-delosreyes <mwldelosreyes13@gmail.com>
2025-07-28 01:16:19 +0800

    refactor: changed null to undefined

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -6,7 +6,7 @@ function ProjectsPage() {
         {
             title: "PokeSet",
             githubUrl: "https://github.com/mawi-delosreyes/PokeSet",
-            liveUrl: null,
+            liveUrl: undefined,
             figmaUrl: "https://www.figma.com/design/VHqXSTM8zLTkyGYHTuuYPM/PokeSet?m=auto&t=3sq8VCpaShcjR1RL-1",
             description: "A Pokemon pokedex and VGC team builder where users can view pokemon details, save their team layouts, and see analytics and team coverages. ",
             languages: ["Java", "React", "TypeScript"],
@@ -15,7 +15,7 @@ function ProjectsPage() {
         {
             title: "Trakua",
             githubUrl: "https://github.com/mawi-delosreyes/Trakua",
-            liveUrl: null,
+            liveUrl: undefined,
             figmaUrl: "https://www.figma.com/design/lKQiwJ35bxSVD7vpDvLHbb/Trakua?m=auto&t=3sq8VCpaShcjR1RL-1",
             description: "A web and mobile application that allows users to track their financial habits and goals. This focuses on the flexibility of tracking from multiple sources.",
             languages: ["Flutter", "Dart"],
@@ -24,8 +24,8 @@ function ProjectsPage() {
         {
             title: "Harvest",
             githubUrl: "https://github.com/mawi-delosreyes/Harvest",
-            liveUrl: null,
-            figmaUrl: null,
+            liveUrl: undefined,
+            figmaUrl: undefined,
             description: "A cryptocurrency algorithmic trader for Coins.ph platform. This connects to their API to execute trades from a 1GB - RAM Raspberry Pi 4 that runs 24/7.",
             languages: ["Python"],
             database: "MySQL"
@@ -33,8 +33,8 @@ function ProjectsPage() {
         {
             title: "Kards",
             githubUrl: "https://github.com/mawi-delosreyes/Kards",
-            liveUrl: null,
-            figmaUrl: null,
+            liveUrl: undefined,
+            figmaUrl: undefined,
             description: "A Baccarat mobile application that tracks game progress and provides analytics for the current turn.",
             languages: ["Dart", "Flutter"],
             database: "SQLite"
